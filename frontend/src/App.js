a1ddfc79708fe34760c6a032bcdca1131bac2ed6
shivam0005 <shivam.verma25004@gmail.com>
2023-07-23 19:15:03 +0530

    db connection made and user registered successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,79 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    balance: 0,
+  });
 
-const App = () => {
-  const [message, setMessage] = useState('');
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
-  const handleClick = () => {
-    // Fetch 'Hello World' message from the backend on button click
-    axios.get('/api/hello')
-      .then((response) => {
-        setMessage(response.data.message);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make a POST request to the Express backend to register the user
+      await axios.post('/api/register', formData);
+      console.log('Registration successful!');
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
   };
 
   return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={handleClick}>Click Me</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="balance">e₹ Balance</label>
+        <input
+          type="number"
+          id="balance"
+          name="balance"
+          value={formData.balance}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
   );
 };
 
-export default App;
+export default RegistrationForm;
